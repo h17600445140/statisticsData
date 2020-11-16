56e245972a0783ae38eb353a660ac3ab46d0b0d2
huangchao <17600445140@163.com>
2020-11-16 23:54:59 +0800

    2020/11/16

--- Test/test.py
+++ Test/test.py
@@ -1,16 +1,57 @@
 import csv
 
 # n = 0
-
 # create_time = 20
 
-with open ('data.csv', newline='', encoding="UTF-8") as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    my_data = [data for data in spamreader]
+# 获取数据
+def getDatas(path):
+    with open (path, newline='', encoding="UTF-8") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        my_datas = [data for data in spamreader]
+    return my_datas
+
+my_datas = getDatas('data.csv')
+
+print(my_datas[0].index("严重程度"))
+
+print(my_datas[0].index("创建日期"))
+
+def getdateNum(condition) -> int:
+    index = my_datas[0].index(condition)
+    count = 0
+    for i in range(1, len(my_datas)):
+        if my_datas[i][index] == "2020-11-13":
+            count = count + 1
+    return count
+
+print(getdateNum("创建日期"))
 
 
-    for index in range(len(my_data[0])):
-        print("{} : {}".format(my_data[0][index], index))
+
+
+
+
+
+
+
+
+# for list in my_data:
+#     for i in range(dict_len):
+#         if list[key[i]] != value[i]:
+#             break
+#         if i == dict_len - 1 and list[key[i]] == value[i]:
+#             result.append(list)
+# result_len = len(result)
+
+# for index in range(len(my_data[0])):
+#     print("{} : {}".format(my_data[0][index], index),end=' , ')
+#
+# for i in range(1,len(my_data)):
+#     print(my_data[i])
+
+
+
+
 
     # print(type(spamreader))
     # print(spamreader.line_num)
@@ -24,12 +65,3 @@ with open ('data.csv', newline='', encoding="UTF-8") as csvfile:
 
 # if __name__ == '__main__':
 #     pass
-
-# a = [1,2,3]
-# b = [4,5,6]
-# for j in b:
-#     print(j)
-#     for i in a:
-#         if i == 2:
-#             break
-#         print(i)
