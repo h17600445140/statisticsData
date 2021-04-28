4d47e2cd3dfcc9d7180e7d8bc90dd5e2a929386d
huangchao <17600445140@163.com>
2021-04-28 18:59:40 +0800

    2021/4/28 优化

--- Tapd/utils.py
+++ Tapd/utils.py
@@ -5,11 +5,11 @@ def getdateNum(my_datas, dict) -> int:
     value = [value for value in dict.values()]
     result = []
 
-    for dict in my_datas:
+    for d in my_datas:
         for i in range(dict_len):
-            if dict[key[i]] != value[i]:
+            if d[key[i]] != value[i]:
                 break
-            if i == dict_len - 1 and dict[key[i]] == value[i]:
+            if i == dict_len - 1 and d[key[i]] == value[i]:
                 result.append(list)
     result_len = len(result)
     return result_len
